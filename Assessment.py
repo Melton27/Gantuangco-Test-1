ee686fce860ab159c9f76b21d00f4f9078609e05
Melton <M.gantuangco.isi@gmail.com>
2019-11-04 14:07:08 +0800

    Assessment

--- Assessment.py
+++ Assessment.py
@@ -1,15 +1,62 @@
-import csv, json
+import csv
+import json
+import sys
+import argparse
+import os.path
+from os import path
+import getopt
 
-csvFilePath = "Sample.csv"
-jsonFilePath = "Sample.json"
+csvfile = "demo.csv"
+jsonfile = "Sample.json"
+
 
 data = {}
 
-with open(csvFilePath) as csvFile:
+with open(csvfile, "r") as csvFile:
     csvReader = csv.DictReader(csvFile)
     for rows in csvReader:
-        id = rows["Year"]
+        id = rows["Name"]
         data[id] = rows
 
-with open(jsonFilePath, "w") as jsonFile:
-    jsonFile.write(json.dumps(data, indent=4))        
+with open(jsonfile, "w") as jsonFile:
+    jsonFile.write(json.dumps(data, indent=4)) 
+    print("Success!")       
+
+import pathlib
+file = pathlib.Path("Sample.json")
+if file.exists ():
+    print ("File exist")
+else:
+    print ("File not exist")
+
+parser = argparse.ArgumentParser(description='AGPARSE!')
+parser.add_argument("--a")
+
+args = parser.parse_args()
+a = args.a
+print(a)
+
+#ef main(argv):
+  # inputfile = 'Sample.csv'
+  # outputfile = 'Sample.json'
+  # try:
+#   opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+  # except getopt.GetoptError:
+#print('Assessment.py -i <Sample.csv> -o <Sample.json>')
+  #    sys.exit(2)
+  # for opt, arg in opts:
+#if opt == '-h':
+#    sys.exit()
+    #  elif opt in ("-i", "--ifile"):
+    #     inputfile = arg
+#elif opt in ("-o", "--ofile"):
+       #  outputfile = arg
+#print ('Input file is "', inputfile)
+ #  print ('Output file is "', outputfile)
+
+#if __name__ == "__main__":
+  # main(sys.argv[1:])
+
+print ('Number of arguments:', len(sys.argv), 'arguments.')
+print ('Argument List:', str(sys.argv))
+
